0c68f2a0803d0abf612ad35c6e64748e768e612a
Andrew Shalankevych <shalankevych@gmail.com>
2015-07-13 21:44:17 +0300

    add switcher for theme's secondary color

--- admin/app/app.js
+++ admin/app/app.js
@@ -1,11 +1,36 @@
-define(['jquery', 'underscore', 'backbone', 'gameboxes/router', 'users/router', 'days/router'],
-	function ($, _, Backbone, GameboxesRouter, UsersRouter, DaysRouter) {
+define(['jquery', 'underscore', 'backbone', 'jsCookie', 'gameboxes/router', 'users/router', 'days/router'],
+	function ($, _, Backbone, Cookies, GameboxesRouter, UsersRouter, DaysRouter) {
 
 	var initialize = function () {
 		new UsersRouter();
 		new GameboxesRouter();
 		new DaysRouter();
 		Backbone.history.start();
+		setUpSideStyles(null);
+		$(".changeSideBtn").click(function (e) {
+			changeSide();
+		});
+	};
+
+	var changeSide = function() {
+		var currentSide = Cookies.get("side");
+		if(currentSide == "dark") {
+			setUpSideStyles("light");
+		} else {
+			setUpSideStyles("dark");
+		}
+	};
+
+	var setUpSideStyles = function(side) {
+		if(side == null) side = Cookies.get("side");
+		if(side == null) side = "light";
+		if(side == "light") {
+			Cookies.set("side", "light");
+			$("#darkStyles").attr("disabled", "disabled");
+		} else {
+			Cookies.set("side", "dark");
+			$("#darkStyles").removeAttr("disabled");
+		}
 	};
 
 	return {
